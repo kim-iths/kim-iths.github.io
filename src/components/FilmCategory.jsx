534950c1b6bc7963d9aaf9774c8b77b7436e6034
Emmeline <emmeline_mutka@hotmail.com>
2021-05-20 19:12:50 +0200

    Fixed styling

--- src/components/FilmCategory.jsx
+++ src/components/FilmCategory.jsx
@@ -9,68 +9,72 @@ const FilmCategory = () => {
 
         return (
 
-        <section className="movieCategory">
-            <h1>Alla filmer</h1>
+        <section className="category-content">
             
-            <div className="categoryContainer">
-            <div className="grid">
-            <div className="col categories"><a href="#" onClick={categoryClick}>
-                <h3>Action</h3>
-            </a></div>
-            <div className="col categories"><a href="#" onClick={categoryClick}>
-                <h3>Animerat</h3>
-            </a></div>
-            <div className="col categories"><a href="#" onClick={categoryClick}>
-                <h3>Barnfilm</h3>
-            </a></div>
-            <div className="col categories"><a href="#" onClick={categoryClick}>
-                <h3>Komedi</h3>
-            </a></div>
-            <div className="col categories"><a href="#" onClick={categoryClick}>
-                <h3>Drama</h3>
-            </a></div>
-            <div className="col categories"><a href="#" onClick={categoryClick}>
-                <h3>Romantik</h3>
-            </a></div>
-            <div className="col categories"><a href="#" onClick={categoryClick}>
-                <h3>Skräck</h3>
-            </a></div>
-            <div className="col categories"><a href="#" onClick={categoryClick}>
-                <h3>Fantasy</h3>
-            </a></div>
-            <div className="col categories"><a href="#" onClick={categoryClick}>
-                <h3>Sci-Fi</h3>
-            </a></div>
-            <div className="col categories"><a href="#" onClick={categoryClick}>
-                <h3>Äventyr</h3>
-            </a></div>
-            <div className="col categories"><a href="#" onClick={categoryClick}>
-                <h3>Thriller</h3>
-            </a></div>
-            <div className="col categories"><a href="#" onClick={categoryClick}>
-                <h3>Dokumentär</h3>
-            </a></div>
-            </div>
+            <div className="movie-category">
+
+                <h1 className="categories-h1">Kategorier</h1>
+                
+                <div className="category-container">
+                    <div className="category-grid">
+                        <button className="category-button col" onClick={categoryClick}>
+                            <h3>Action</h3>
+                        </button>
+                        <button className ="category-button col" onClick={categoryClick}>
+                            <h3>Animerat</h3>
+                        </button>
+                        <button className ="category-button col" onClick={categoryClick}>
+                            <h3>Barnfilm</h3>
+                        </button>
+                        <button className ="category-button col" onClick={categoryClick}>
+                            <h3>Komedi</h3>
+                        </button>
+                        <button className ="category-button col" onClick={categoryClick}>
+                            <h3>Drama</h3>
+                        </button>
+                        <button className ="category-button col" onClick={categoryClick}>
+                            <h3>Romantik</h3>
+                        </button>
+                        <button className ="category-button col" onClick={categoryClick}>
+                            <h3>Skräck</h3>
+                        </button>
+                        <button className ="category-button col" onClick={categoryClick}>
+                            <h3>Fantasy</h3>
+                        </button>
+                        <button className ="category-button col" onClick={categoryClick}>
+                            <h3>Sci-Fi</h3>
+                        </button>
+                        <button className ="category-button col" onClick={categoryClick}>
+                            <h3>Äventyr</h3>
+                        </button>
+                        <button className ="category-button col" onClick={categoryClick}>
+                            <h3>Thriller</h3>
+                        </button>
+                        <button className ="category-button col" onClick={categoryClick}>
+                            <h3>Dokumentär</h3>
+                        </button>
+                    </div>
+                </div>
             </div>
 
-            <h3>Alla filmer</h3>
-            <div className="suggestionContainer">
-            <div className="grid">
-            <div className="col suggestion"></div>
-            <div className="col suggestion"></div>
-            <div className="col suggestion"></div>
-            <div className="col suggestion"></div>
-            <div className="col suggestion"></div>
-            <div className="col suggestion"></div>
-            <div className="col suggestion"></div>
-            <div className="col suggestion"></div>
-            <div className="col suggestion"></div>
-            <div className="col suggestion"></div>
-            </div>
+            <h3 className="all-movies-h3">Alla filmer</h3>
+
+            <div className="all-movies-container">
+                <div className="all-movies-grid">
+                    <div className="col all-movies"></div>
+                    <div className="col all-movies"></div>
+                    <div className="col all-movies"></div>
+                    <div className="col all-movies"></div>
+                    <div className="col all-movies"></div>
+                    <div className="col all-movies"></div>
+                    <div className="col all-movies"></div>
+                    <div className="col all-movies"></div>
+                    <div className="col all-movies"></div>
+                    <div className="col all-movies"></div>
+                </div>
             </div>
         </section>
         )
-
 }
 
 export default FilmCategory;
