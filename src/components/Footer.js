7bf4049d9ecd47c240514e46ce447c009f317c09
David Savenmark <dsinemark@gmail.com>
2021-05-21 11:16:10 +0200

    added copyright sign to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <div className="footer" id="footer-text">
-    <p>Movieblock AB 2021 support@movieblock.se</p>
+    <p> © Movieblock AB 2021 support@movieblock.se</p>
   </div>
 );
 
