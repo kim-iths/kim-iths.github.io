0dc076fd0c33e1fa4af1090198f373476da90df8
kim-iths <kim.hellman@iths.se>
2021-05-25 13:24:35 +0200

    Styled somewhat

--- src/App.js
+++ src/App.js
@@ -33,11 +33,8 @@ function App() {
         </p>
       </header>
       <main>
-        <p>
-          Funkar jag?
-        </p>
-        <img src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcQQt0kUCWldIeDC3LgW98DLQ0qBWgfprfeiFdQzDMtdtwSzwxeNOivQWFqoUvLYlNYnF716-PiaHTTKdTVv0hxvytbmvR6VCvrY_YSPvrkwDCVbwlAMejXx5g&usqp=CAE" alt="dog" />
-        <p>a</p>
+        
+        <Filminfo />
 
       <script src="https://www.gstatic.com/firebasejs/8.6.1/firebase-app.js"></script>
 
