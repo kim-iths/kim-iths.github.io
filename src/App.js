f123aa33f26c5ba8f471d9e4c35fc43f7c97e8d6
MinjaCheng <minja.cheng@iths.se>
2021-05-27 23:45:15 +0200

    Fix footer

--- src/App.js
+++ src/App.js
@@ -98,7 +98,7 @@ function App() {
 
       <footer>
         <div className="footer" id="footer-text">
-          <p> © Movieblock AB 2021 support@movieblock.se</p>
+          <p id="footer-text"> © Movieblock AB 2021 support@movieblock.se</p>
         </div>
       </footer>
     </div>
