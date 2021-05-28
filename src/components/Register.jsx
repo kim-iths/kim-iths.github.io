0fbbe3200c4dc9a4938fc0064331fbcfec18bfa7
MinjaCheng <minja.cheng@iths.se>
2021-05-28 12:47:57 +0200

    Remove line 4

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,6 @@
 import './register.css';
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
-//going to write code here, test and commit 0528
 
 
 const Register = () => {
