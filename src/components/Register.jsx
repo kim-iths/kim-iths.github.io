1416c302e534b3d5456e1b973c3f8ee3bfdbee32
Wenjing81 <xxnfdg@gmail.com>
2021-05-28 12:06:00 +0200

    test and commit 0528

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
-//going to write code here 111
+//going to write code here, test and commit 0528
+
 const Register = () => {
 
 }
