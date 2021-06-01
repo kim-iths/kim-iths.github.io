eac047221a592754e04ad28814e634eaa0173045
MinjaCheng <minja.cheng@iths.se>
2021-06-01 12:38:04 +0200

    Be able to see images and titles

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -8,126 +8,65 @@ import { Link } from "react-router-dom";
 const StartScreen = () => {
 
     const [newMovies, setNewMovies] = useState([]);
+    const [popularMovies, setPopularMovies] = useState([]);
+    const [slides, setSlides] = useState([]);
+    const [slidesAreCreated, setSlidesAreCreated] = useState(false);
 
     useEffect(async () => {
-        let url = "https://api.themoviedb.org/3/movie/550?api_key=86f237d170416093156de7affa43927e";
-        let dataMovies = await GetData(url);
-        console.log('3. Got data in startscreen', dataMovies);
-        setNewMovies(dataMovies);
+        let url = "https://api.themoviedb.org/3/movie/now_playing?api_key=86f237d170416093156de7affa43927e";
+        let data = await GetData(url);
+        console.log('3. Got data in startscreen', data.results);
+        setNewMovies(data.results);
+        createSlides();
     }, [])
 
+    useEffect(async () => {
+        let url = "https://api.themoviedb.org/3/movie/popular?api_key=86f237d170416093156de7affa43927e";
+        let data = await GetData(url);
+        console.log('3. Got data in startscreen', data.results);
+        setPopularMovies(data.results);
+    }, [])
 
-    let imageUrl = `https://image.tmdb.org/t/p/w500${newMovies.backdrop_path}`;
+    function createSlides() {
+        let slide = newMovies.map((newMovie, index) => (
+            <div key={index} className="movie-slide-div">
+                <Link to="/filminfo" style={{ textDecoration: 'none' }}>
+                    <img src={`https://image.tmdb.org/t/p/original${newMovie.backdrop_path}`} alt={newMovie.original_title} />,
+            <p className="movie-slide-title">{newMovie.original_title}</p>,
+            <aside className="movie-slide-heart">&#x2665;</aside>,
+            </Link>
+            </div>
+        ));
+        setSlides(slide);
+        setSlidesAreCreated(true)
+    }
 
-    let slides = [
-        <div className="movie-slide-div">
-            <img src={imageUrl} alt="title" />
-            <p className="movie-slide-title">{newMovies.original_title}</p>
-            <aside className="movie-slide-heart">&#x2665;</aside>
-        </div>,
-        <div className="movie-slide-div">
-            <img src="img/the_lego_movie.png" alt="title" />,
-            <p className="movie-slide-title">The lego movie</p>
-            <aside className="movie-slide-heart">&#x2665;</aside>
-        </div>,
-        <div className="movie-slide-div">
-            <img src="img/black_panther.png" alt="title" />
-            <p className="movie-slide-title">Black Panther</p>
-            <aside className="movie-slide-heart">&#x2665;</aside>
-        </div>,
-        <div className="movie-slide-div">
-            <img src="img/the_lego_movie.png" alt="title" />,
-            <p className="movie-slide-title">The lego movie</p>
-            <aside className="movie-slide-heart">&#x2665;</aside>
-        </div>,
-        <div className="movie-slide-div">
-            <img src="img/black_panther.png" alt="title" />
-            <p className="movie-slide-title">Black Panther</p>
-            <aside className="movie-slide-heart">&#x2665;</aside>
-        </div>,
-        <div className="movie-slide-div">
-            <img src="img/the_lego_movie.png" alt="title" />,
-            <p className="movie-slide-title">The lego movie</p>
-            <aside className="movie-slide-heart">&#x2665;</aside>
-        </div>,
-    ];
-
-    return (
-        <div className="startscreen-wrap">
-
-            <section id="movie-slider">
-                <h2 className="title-new-movies">Nya filmer</h2>
-                <Carousel slides={slides} autoplay={false} interval={5000} />
-            </section>
-            <section id="popular-movies">
-                <h3 className="title-popular-movies">Populära filmer</h3>
-                <div className="grids-popular-movies">
-                    <Link to="/filminfo">
-                        <div className="popular-movies-div">
-                            <img src="img/black_panther.png" alt="title" />
-                            <p>Black Panther</p>
-                            <aside className="movie-heart">&#x2665;</aside>
+    if (slidesAreCreated) {
+        return (
+            <div className="startscreen-wrap">
+                <section id="movie-slider">
+                    <h2 className="title-new-movies">Nya filmer</h2>
+                   <Carousel slides={slides} autoplay={true} interval={5000} />
+                </section>
+                <section id="popular-movies">
+                    <h3 className="title-popular-movies">Populära filmer</h3>
+                    <div className="popular-movies-container">
+                        <div className="grids-popular-movies">
+                            {popularMovies.map((popularMovie, index) => (
+                                <Link to="/filminfo" style={{ textDecoration: 'none' }}>
+                                    <div key={index} className="popular-movies-div">
+                                        <img src={`https://image.tmdb.org/t/p/w500${popularMovie.poster_path}`} alt={popularMovie.original_title} />
+                                        <p>{popularMovie.original_title}</p>
+                                        <aside className="movie-heart">&#x2665;</aside>
+                                    </div>
+                                </Link>
+                            ))}
                         </div>
-                    </Link>
-                    <div className="popular-movies-div">
-                        <img src="img/the_lego_movie.png" alt="title" />
-                        <p>The Lego Movie</p>
-                        <aside className="movie-heart">&#x2665;</aside>
                     </div>
-                    <div className="popular-movies-div">
-                        <img src="img/black_panther.png" alt="title" />
-                        <p>Black Panther</p>
-                        <aside className="movie-heart">&#x2665;</aside>
-                    </div>
-                    <div className="popular-movies-div">
-                        <img src="img/the_lego_movie.png" alt="title" />
-                        <p>The Lego Movie</p>
-                        <aside className="movie-heart">&#x2665;</aside>
-                    </div>
-                    <div className="popular-movies-div">
-                        <img src="img/black_panther.png" alt="title" />
-                        <p>Black Panther</p>
-                        <aside className="movie-heart">&#x2665;</aside>
-                    </div>
-                    <div className="popular-movies-div">
-                        <img src="img/the_lego_movie.png" alt="title" />
-                        <p>The Lego Movie</p>
-                        <aside className="movie-heart">&#x2665;</aside>
-                    </div>
-                    <div className="popular-movies-div">
-                        <img src="img/black_panther.png" alt="title" />
-                        <p>Black Panther</p>
-                        <aside className="movie-heart">&#x2665;</aside>
-                    </div>
-                    <div className="popular-movies-div">
-                        <img src="img/the_lego_movie.png" alt="title" />
-                        <p>The Lego Movie</p>
-                        <aside className="movie-heart">&#x2665;</aside>
-                    </div>
-                    <div className="popular-movies-div">
-                        <img src="img/black_panther.png" alt="title" />
-                        <p>Black Panther</p>
-                        <aside className="movie-heart">&#x2665;</aside>
-                    </div>
-                    <div className="popular-movies-div">
-                        <img src="img/the_lego_movie.png" alt="title" />
-                        <p>The Lego Movie</p>
-                        <aside className="movie-heart">&#x2665;</aside>
-                    </div>
-                    <div className="popular-movies-div">
-                        <img src="img/black_panther.png" alt="title" />
-                        <p>Black Panther</p>
-                        <aside className="movie-heart">&#x2665;</aside>
-                    </div>
-                    <div className="popular-movies-div">
-                        <img src="img/the_lego_movie.png" alt="title" />
-                        <p>The Lego Movie</p>
-                        <aside className="movie-heart">&#x2665;</aside>
-                    </div>
-                </div>
-            </section>
-        </div>
-    )
+                </section>
+            </div>
+        )
+    } else return ("Loading...")
 }
 
 export default StartScreen;
