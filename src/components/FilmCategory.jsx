64be8c74637951e678b93668fc028c303edd672b
Emmeline <emmeline_mutka@hotmail.com>
2021-06-01 14:26:11 +0200

    Fixed so you can change category and fixed so you can press the next page button and get the next page

--- src/components/FilmCategory.jsx
+++ src/components/FilmCategory.jsx
@@ -3,42 +3,40 @@ import React, { Component, useState } from 'react'
 import { Link } from "react-router-dom";
 import { useEffect } from 'react';
 import GetData from './GetData'
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 
 const FilmCategory = () => {
 
     const [categoryList, setCategoryList] = useState([]);
-    
-    let pageNumber = 1;
+    const [pageNumber, setPageNumber] = useState(1);
+    const [categoryId, setCategoryId] = useState(35);
+    useEffect(() => {
+        loadCategory(categoryId)
+    }, [pageNumber, categoryId])
 
     let loadCategory = async (categoryId) => {
-        let url = `https://api.themoviedb.org/3/discover/movie?api_key=86f237d170416093156de7affa43927e&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pageNumber}&with_genres=${categoryId}`;
+        let url = `https://api.themoviedb.org/3/discover/movie?api_key=86f237d170416093156de7affa43927e&sort_by=vote_count.desc&include_adult=false&include_video=false&page=${pageNumber}&with_genres=${categoryId}`;
         let categoryMovies = await GetData(url);
         if(categoryMovies != null) {
             setCategoryList(
-                ...categoryList,
                 categoryMovies.results
             );
         }
         console.log('results: ', categoryMovies.results);
         console.log('loadCategory: ', categoryMovies);
     }
-
-    function categoryClick(e) {
-        e.preventDefault();
-        console.log('The button was clicked.');
-    }
-    
-    let genreList = categoryList.map((category, index) => {
+    let moviesCategoryList = categoryList.map((category, index) => {
         return (
             categoryList != [] ?
             <div key={index} className="all-categories">
                 {category.poster_path ? 
                     <img src={`https://image.tmdb.org/t/p/w500${category.poster_path}`} className="category-poster" alt="" />
                 :
-                    <p>Poster unavailable</p>
+                <img src="img/no-poster.png" alt="" />
             }
                 <p>{category.title}</p>
-                <aside className="all-categories-heart">&#x2665;</aside>
+                <aside className="all-categories-heart"><IconButton aria-label="favorite"><FavoriteIcon style={{ backgroundColor:'#25252594', borderRadius: '20%', fill: '#ffffff' }} /></IconButton></aside> 
             </div>
             :
             <p key={index}>No data</p>
@@ -53,40 +51,40 @@ const FilmCategory = () => {
                 <h1 className="categories-h1">Kategorier</h1>
 
                 <div className="category-grid">
-                    <button className="category-button" onClick={() => loadCategory(28)}>
+                    <button className="category-button" onClick={() => setCategoryId(28)}>
                         <h3>Action</h3>
                     </button>
-                    <button className="category-button" onClick={() => loadCategory(16)}>
+                    <button className="category-button" onClick={() => setCategoryId(16)}>
                         <h3>Animerat</h3>
                     </button>
-                    <button className="category-button" onClick={categoryClick}>
+                    <button className="category-button" onClick={() => setCategoryId(10751)}>
                         <h3>Barnfilm</h3>
                     </button>
-                    <button className="category-button" onClick={categoryClick}>
+                    <button className="category-button" onClick={() => setCategoryId(35)}>
                         <h3>Komedi</h3>
                     </button>
-                    <button className="category-button" onClick={categoryClick}>
+                    <button className="category-button" onClick={() => setCategoryId(18)}>
                         <h3>Drama</h3>
                     </button>
-                    <button className="category-button" onClick={categoryClick}>
+                    <button className="category-button" onClick={() => setCategoryId(10749)}>
                         <h3>Romantik</h3>
                     </button>
-                    <button className="category-button" onClick={categoryClick}>
+                    <button className="category-button" onClick={() => setCategoryId(27)}>
                         <h3>Skräck</h3>
                     </button>
-                    <button className="category-button" onClick={categoryClick}>
+                    <button className="category-button" onClick={() => setCategoryId(14)}>
                         <h3>Fantasy</h3>
                     </button>
-                    <button className="category-button" onClick={categoryClick}>
+                    <button className="category-button" onClick={() => setCategoryId(878)}>
                         <h3>Science Fiction</h3>
                     </button>
-                    <button className="category-button" onClick={categoryClick}>
+                    <button className="category-button" onClick={() => setCategoryId(12)}>
                         <h3>Äventyr</h3>
                     </button>
-                    <button className="category-button" onClick={categoryClick}>
+                    <button className="category-button" onClick={() => setCategoryId(53)}>
                         <h3>Thriller</h3>
                     </button>
-                    <button className="category-button" onClick={categoryClick}>
+                    <button className="category-button" onClick={() => setCategoryId(99)}>
                         <h3>Dokumentär</h3>
                     </button>
                 </div>
@@ -96,10 +94,13 @@ const FilmCategory = () => {
 
             <div className="all-categories-container">
                 <div className="all-categories-grid">
-                        {genreList}
+                    {moviesCategoryList}
                 </div>
             </div>
-
+                <button className="next-page-button" onClick={() => 
+                    setPageNumber(pageNumber+1)}>
+                    NÄSTA SIDA
+                </button>
         </section>
     )
 }
