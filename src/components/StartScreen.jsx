f97488ae64e0bbcddb7c78d4902cabbf19e202f0
MinjaCheng <minja.cheng@iths.se>
2021-06-01 22:36:06 +0200

    Done with StartScreen

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -10,6 +10,7 @@ const StartScreen = () => {
 
     const [newMovies, setNewMovies] = useState([]);
     const [popularMovies, setPopularMovies] = useState([]);
+    const [totalPages, setTotalPages] = useState(0);
     const [slides, setSlides] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
 
@@ -19,17 +20,18 @@ const StartScreen = () => {
         console.log('3. Got data in startscreen', data.results);
         setNewMovies(data.results);
     }, [])
-    
+
     useEffect(async () => {
         let url = `https://api.themoviedb.org/3/movie/popular?api_key=86f237d170416093156de7affa43927e&page=${currentPage}`;
         let data = await GetData(url);
-        console.log('3. Got data in startscreen', data.results);
+        console.log('4. Got data in startscreen', data.results);
         setPopularMovies(data.results);
-    }, [])
+        setTotalPages(data.total_pages);
+    }, [currentPage])
 
     useEffect(() => {
         createSlides();
-    },[newMovies])
+    }, [newMovies])
 
     function createSlides() {
         let slide = newMovies.map((newMovie, index) => (
@@ -42,7 +44,7 @@ const StartScreen = () => {
             </div>
         ));
         setSlides(slide);
-        
+
     }
     console.log("slides:", slides)
 
@@ -69,13 +71,33 @@ const StartScreen = () => {
                         </div>
                     </div>
                     <div className="start-screen-buttons">
-                        <div className="change-page-div"><button className="change-page-button"> {"< Föregående sida"}</button></div>
-                        <div className="change-page-div"><button className="change-page-button"> {"Nästa sida >"} </button></div>
+                        <div className="change-page-div">
+                            <button className="change-page-button" onClick={previousPage}>
+                                {"< Föregående sida"}
+                            </button>
+                        </div>
+                        <div className="change-page-div">
+                            <button className="change-page-button" onClick={nextPage}>
+                                {"Nästa sida >"}
+                            </button>
+                        </div>
                     </div>
                 </section>
             </div>
         )
     } else return (<h3 className="loading-text">Loading...</h3>)
+
+    function nextPage() {
+        if (currentPage < totalPages) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
+
+    function previousPage() {
+        if (currentPage > 1) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
 }
 
 export default StartScreen;
